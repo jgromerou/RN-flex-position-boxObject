9cd45129e64a48d8c15800aa9b7c2f2c2776dc91
jgromerou <jgromerouro@gmail.com>
2023-10-03 22:25:34 -0300

    se hizo la tarea 1.

--- App.tsx
+++ App.tsx
@@ -4,18 +4,22 @@ import { BoxObjectModelScreen } from './src/screen/BoxObjectModelScreen';
 import { DimensionesScreen } from './src/screen/DimensionesScreen';
 import PositionScreen from './src/screen/PositionScreen';
 import { FlexScreen } from './src/screen/FlexScreen';
-import { TareasSceen } from './src/screen/TareasSceen';
+import { Tarea1Sceen } from './src/screen/Tarea1Sceen';
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex: 1, 
-                          backgroundColor: '#28425b', 
-                          paddingTop: Platform.OS === 'android' ? 24: 0}}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: '#28425b',
+        paddingTop: Platform.OS === 'android' ? 24 : 0,
+      }}
+    >
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionesScreen /> */}
       {/* <PositionScreen /> */}
       {/* <FlexScreen /> */}
-      <TareasSceen />
+      <Tarea1Sceen />
     </SafeAreaView>
   );
 }
